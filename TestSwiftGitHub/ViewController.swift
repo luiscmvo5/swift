8bd4e86d283ccc9e758dd40b7d51a76450ef943d
Luis Castellanos Morales <luiscmvo5@gmail.com>
2015-12-06 22:14:40 -0600

    Initial Commit

---
+++ TestSwiftGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSwiftGitHub
+//
+//  Created by Luis Castellanos Morales on 06/12/15.
+//  Copyright © 2015 Luis Castellanos Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
